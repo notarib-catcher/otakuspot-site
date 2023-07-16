b00688e3940a7b0e143b2d8978f4bb7d3869cbb2
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-07-16 23:25:35 +0530

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import otakuBG from "./assets/otakubg.svg"
+  import comicBG from "./assets/bg.webp"
+</script>
+<div class="w-screen h-screen">
+    <img src = {otakuBG} class="h-screen w-screen object-cover fixed top-0 left-0 z-10">
+    <img src = {comicBG} class="h-screen w-screen object-cover fixed top-0 left-0 z-0 brightness-[30%]">
+</div>
+
+<slot />
+
+<style>
+
+</style>
