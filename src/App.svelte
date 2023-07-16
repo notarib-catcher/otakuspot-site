6a07d8fa22afc072bad958365fb1d3d7d31f3922
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-07-16 23:57:42 +0530

    Mobile friendly resizing!

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,9 @@
   import otakuBG from "./assets/otakubg.svg"
   import comicBG from "./assets/bg.webp"
 </script>
-<div class="w-screen h-screen">
-    <img src = {otakuBG} class="h-screen w-screen object-cover fixed top-0 left-0 z-10">
-    <img src = {comicBG} class="h-screen w-screen object-cover fixed top-0 left-0 z-0 brightness-[30%]">
+<div class="w-screen h-screen flex flex-col items-center justify-center">
+    <img src = {otakuBG} class="md:h-screen w-screen object-cover top-0 left-0 z-10" alt="Otaku logo">
+    <img src = {comicBG} class="h-screen w-screen object-cover fixed top-0 left-0 z-0 brightness-[30%]" alt="Cominc BG">
 </div>
 
 <slot />
