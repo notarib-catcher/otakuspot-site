92388b1ec3070bddc96d44e870de53f0456bc98b
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-07-17 22:14:56 +0530

    Added socials and some animation

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,35 @@
 <script>
-  import otakuBG from "./assets/otakubg.svg"
-  import comicBG from "./assets/bg.webp"
+    import CC from "./lib/CenterComponent.svelte"
+    import comicBG from "./assets/bg.webp"
 </script>
 <div class="w-screen h-screen flex flex-col items-center justify-center">
-    <img src = {otakuBG} class="md:h-screen w-screen object-cover top-0 left-0 z-10" alt="Otaku logo">
-    <img src = {comicBG} class="h-screen w-screen object-cover fixed top-0 left-0 z-0 brightness-[30%]" alt="Cominc BG">
+
+    <CC/>
+
+
+    <img src = {comicBG} class="h-screen w-screen object-cover fixed top-0 left-0 z-0 fadeinBGAnim" alt="Cominc BG">
 </div>
 
 <slot />
 
 <style>
+
+
+    @keyframes fadeinBG {
+        0%{
+            filter: brightness(100%);
+        }
+        100%{
+            filter: brightness(30%);
+        }
+    }
+
+    .fadeinBGAnim{
+        animation: fadeinBG ease 300ms;
+        animation-iteration-count: 1;
+        animation-fill-mode: forwards;
+    }
+
+
 
 </style>
